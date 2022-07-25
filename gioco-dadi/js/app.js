6606a48e7a8405784f0a1a42866f5359f815637c
Sansone11 <sansonemirco@gmail.com>
2022-07-25 16:41:02 +0200

    dadi-edit

---
+++ gioco-dadi/js/app.js
@@ -0,0 +1,15 @@
+console.log('JS ok');
+// crea un generatore di numeri casuali bot
+// crea un genetore di nume casuali user
+// attribuisci a entrambi valore minimo e massimo da 1 a 6
+// specificare che siamo numeri interi
+// se il valore casuale del bot è <= del valore
+// stampa hai perso
+// altrimenti stampa congratulazioni hai vinto
+let bot = 1+Math.floor(6*Math.random());
+console.log(bot);
+let user = 1+Math.floor(6*Math.random()); 
+console.log(bot);
+document.getElementById("bot").innerHTML ="bot ";
+	document.getElementById("user").innerHTML ="user";
+    // document.addEventListener("click");
